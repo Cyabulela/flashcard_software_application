1c7b1790eb4edc9b2e7890486202c8e389bd6cf9
Cyabulela <95098257+Cyabulela@users.noreply.github.com>
2023-05-08 01:27:00 +0200

    commit full database and id to integer

---
+++ app/src/main/java/com/example/flashcardsoftwareapplication/data/repository/FlashCardSQLiteDatabaseRepository.java
@@ -0,0 +1,40 @@
+package com.example.flashcardsoftwareapplication.data.repository;
+
+import android.content.Context;
+
+import com.example.flashcardsoftwareapplication.data.dto.FlashCardDto;
+import com.example.flashcardsoftwareapplication.data.local.FlashCardDatabase;
+import com.example.flashcardsoftwareapplication.data.mappers.ListMappers;
+import com.example.flashcardsoftwareapplication.domain.modelling.FlashCard;
+import com.example.flashcardsoftwareapplication.domain.repository.FlashCardRepository;
+
+import java.util.List;
+
+public class FlashCardRepositoryImpl implements FlashCardRepository {
+
+    private final FlashCardDatabase database;
+
+    public FlashCardRepositoryImpl(Context context) {
+        database = new FlashCardDatabase(context);
+    }
+    @Override
+    public void insectFlashCard(FlashCard flashCard) {
+        database.insectFlashcard(flashCard.toFlashCardDto());
+    }
+
+    @Override
+    public void updateFlashCard(FlashCard flashCard) {
+        database.updateFlashcard(flashCard.toFlashCardDto());
+    }
+
+    @Override
+    public boolean deleteFlashCard(FlashCard flashCard) {
+        return database.deleteFlashcard(flashCard.toFlashCardDto());
+    }
+
+    @Override
+    public List<FlashCard> getFlashCards() {
+        List<FlashCardDto> flashCardDtos = database.getAllFlashcards();
+        return ListMappers.toFlashCardList(flashCardDtos);
+    }
+}
